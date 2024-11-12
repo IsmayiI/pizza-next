60871ab426af9e5552969cfdfb6c64073e551bd7
Ismayil <ismayilov_ismayil_97@mail.ru>
2024-11-12 19:37:26 +0400

    add query to filters

--- services/ingredients.ts
+++ services/ingredients.ts
@@ -3,7 +3,7 @@ import { axiosInstance } from "./instance"
 import { ApiRoutes } from "./constants"
 
 export const getAll = async () => {
-   const { data } = await axiosInstance.get<Ingredient[]>(ApiRoutes.GET_INGREDIENTS)
+   const { data } = await axiosInstance.get<Ingredient[]>(ApiRoutes.INGREDIENTS)
 
    return data
 }
